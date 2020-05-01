11dad5e966ab64f186c9bc6952d43f2e6b185176
Desarrolloglj <desarrolloglj@gmail.com>
2020-05-01 19:15:08 -0300

    SPA part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,21 +4,30 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
         };
     }
 
     render() {
         const HomePage = () => {
             return(
-                <Home></Home>
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}  
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]} ></Home>
             )
         }
         
@@ -27,6 +36,7 @@ class Main extends Component {
             <Header />
             <Switch>
                 <Route path="/home" component={HomePage} />
+                <Route exact path="/contactus" component={Contact} />
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                 <Redirect to="/home" />
             </Switch>
